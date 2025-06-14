8d93e80dfd6c69b539aa24f6bbd7752e53f706bd
cristian1922 <cristian1922cl@gmail.com>
2025-06-14 10:17:07 -0500

    Update app.config.ts
    ddd

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,4 +6,4 @@ import { routes } from './app.routes';
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
 };
-//prueba 2
+//prueba 3
